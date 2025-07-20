96ccfb05e44b886c661d9787f2e10f554581ea95
William de Witt <sasilverain@hotmail.com>
2025-07-20 16:10:24 +0200

    feat: Add initial HealthTracker solution and integration tests

---
+++ test/HealthTracker.AppHost.Tests/IntegrationTest1.cs
@@ -0,0 +1,37 @@
+namespace HealthTracker.AppHost.Tests;
+
+public class IntegrationTest1
+{
+  [Fact]
+  public async Task GetWebResourceRootReturnsOkStatusCode()
+  {
+    // Arrange
+    var builder = await DistributedApplicationTestingBuilder
+        .CreateAsync<Projects.HealthTracker_AppHost>();
+
+    builder.Services.ConfigureHttpClientDefaults(clientBuilder =>
+    {
+      clientBuilder.AddStandardResilienceHandler();
+    });
+
+    // To output logs to the xUnit.net ITestOutputHelper, 
+    // consider adding a package from https://www.nuget.org/packages?q=xunit+logging
+
+    await using var app = await builder.BuildAsync();
+
+    await app.StartAsync();
+
+    // Act
+    var httpClient = app.CreateHttpClient("webfrontend");
+
+    using var cts = new CancellationTokenSource(TimeSpan.FromSeconds(30));
+    await app.ResourceNotifications.WaitForResourceHealthyAsync(
+        "webfrontend",
+        cts.Token);
+
+    var response = await httpClient.GetAsync("/");
+
+    // Assert
+    Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+  }
+}
